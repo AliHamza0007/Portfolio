9e4b7610ed3dddfa2aea96b036f845480261f038
AliHamza0007 <141808379+AliHamza0007@users.noreply.github.com>
2024-04-19 14:53:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,'./client/build')));
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
-app.get('*',function(req,res){
+app.use('*',function(req,res){
 res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
 //port
